828b4a6fe7516d651fe4c974a939dc3c4501792b
eima-sl <eimalive@gmail.com>
2018-08-14 01:25:57 +0530

    bs4 BeautifulSoup scrape tags

--- scrTest.py
+++ scrTest.py
@@ -1,3 +1,9 @@
+#from urllib.request import  urlopen
+#html = urlopen("http://vertspace.lk/work.html")
+#print(html.read())
+
 from urllib.request import  urlopen
+from bs4 import BeautifulSoup
 html = urlopen("http://vertspace.lk/work.html")
-print(html.read())
+bsObj = BeautifulSoup(html.read(),'lxml')
+print(bsObj.h1)
