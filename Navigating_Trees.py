4809fc9a82c459626a5366f6b585afd5ac9fa91f
eima-sl <eimalive@gmail.com>
2018-08-14 17:04:29 +0530

    Navigating Trees / sibilings,parents,childrens 07

--- Navigating_Trees.py
+++ Navigating_Trees.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 html= urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html,"lxml")
 
-print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
-
 for child in bsObj.find("table",{"id":"giftList"}).children:
      print(child)
 
@@ -16,3 +14,4 @@ for child1 in bsObj.find("table",{"id":"giftList"}).descendants:
 for sibling in bsObj.find("table",{"id":"giftList"}).tr.next_siblings:
      print(sibling)
 
+print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
